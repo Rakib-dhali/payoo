eee80a922473858ca4c17b5bdfb74ca4db082f24
Rakib Dhali <rakibdhali343@gmail.com>
2026-02-17 23:42:20 +0600

    login page

---
+++ scripts/login.js
@@ -0,0 +1,19 @@
+const number = "01234567890";
+const pin = "1234";
+
+document.getElementById("login-btn").addEventListener("click", function () {
+  const inputNumber = document.getElementById("number").value;
+  const inputPin = document.getElementById("pin").value;
+  if (inputNumber === number && inputPin === pin) {
+    alert("logged in successfully");
+    window.location.replace("/home.html");
+  } else {
+    if (inputNumber === "" || inputPin == "") {
+      alert("number or pin required");
+    } else if (inputNumber.length < 11) {
+      alert(`login failed .number must be 11 digit`);
+    } else if (inputPin < 4) {
+      alert("login failed. pin must be 4 digit");
+    }
+  }
+});
